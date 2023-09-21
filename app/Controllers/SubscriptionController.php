63e7cc45cfdf8183e4dc10d8aa704cd9f9b70422
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2023-09-21 22:06:08 +0700

    create controller

---
+++ app/Controllers/SubscriptionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class SubscriptionController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Controllers/TransactionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class TransactionController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
