421bf6b49b9b1c612f95828340efe3b6c305d11f
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2024-07-18 07:22:15 +0700

    test api on hosting

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/test', 'Home::index');
 
 // Api Mobile
 $routes->group('api/v1', ["filter" => 'cors'],  function ($routes) {
